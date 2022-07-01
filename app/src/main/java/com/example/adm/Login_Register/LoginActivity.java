6f1e99e06fcc3bb299e7d29d3e8cc02390504600
postbox-18 <aravinthc18@gmail.com>
2022-07-01 09:55:35 +0530

    Updated Forgot Password

--- app/src/main/java/com/example/adm/Login_Register/LoginActivity.java
+++ app/src/main/java/com/example/adm/Login_Register/LoginActivity.java
@@ -5,12 +5,14 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.InputType;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.AutoCompleteTextView;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,6 +33,7 @@ import com.example.adm.MainActivity;
 import com.example.adm.R;
 import com.example.adm.ViewModel.GetViewModel;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -42,7 +45,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText email;
     private AutoCompleteTextView password;
     private AppCompatButton login_btn;
-    private TextView no_account;
+    private TextView no_account,forgot;
     private String s_email, s_password;
     private String TAG = "LoginActivity";
     private CheckBox remember_me;
@@ -77,6 +80,7 @@ public class LoginActivity extends AppCompatActivity {
         lottie_loading = findViewById(R.id.lottie_loading);
         no_account = findViewById(R.id.no_account);
         remember_me = findViewById(R.id.remember_me);
+        forgot = findViewById(R.id.forgot);
 
         firebaseDatabase = FirebaseDatabase.getInstance();
 
@@ -154,8 +158,80 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-    }
+        //onclick forgot password
+        //forgot on click
+        forgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(LoginActivity.this);
+                builder.setTitle("Recover Password");
+                LinearLayout linearLayout=new LinearLayout(LoginActivity.this);
+                final EditText emailet= new EditText(LoginActivity.this);
 
+                // write the email using which you registered
+                if((email.getText().toString()).isEmpty())
+                {
+                    emailet.setHint("Email");
+                }
+                else {
+                    emailet.setText(email.getText().toString());
+                }
+                emailet.setMinEms(16);
+                emailet.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+                linearLayout.addView(emailet);
+                linearLayout.setPadding(10,10,10,10);
+                builder.setView(linearLayout);
+
+                // Click on Recover and a email will be sent to your registered email id
+                builder.setPositiveButton("Recover", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String email=emailet.getText().toString().trim();
+                        beginRecovery(email);
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.create().show();
+            }
+        });
+
+
+    }
+    private void beginRecovery(String email) {
+
+        loadingDialog.show(getSupportFragmentManager(), "Loading dailog");
+
+        // calling sendPasswordResetEmail
+        // open your email and write the new
+        // password and then you can login
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                loadingDialog.dismiss();
+                if(task.isSuccessful())
+                {
+                    // if isSuccessful then done message will be shown
+                    // and you can change the password
+                    Toast.makeText(LoginActivity.this,"Done sent",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(LoginActivity.this,"Error Occurred",Toast.LENGTH_LONG).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                loadingDialog.dismiss();
+                Toast.makeText(LoginActivity.this,"Error Failed",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
     private void Auth() {
         mAuth.signInWithEmailAndPassword(s_email, s_password)
                 .addOnCompleteListener(
